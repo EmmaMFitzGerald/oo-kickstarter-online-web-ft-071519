1f6bed22344e0dfcec93fcf89609fa005bb62131
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-02 15:22:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
   
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects, :backers
   
   def initialize(name)
     @name = name
@@ -9,13 +9,13 @@ class Backer
   
   def back_project(project)
     @backed_projects <<  project
-    
+    @backers <<  project
   end
   
   
 end 
-But... do you see the problem? Both sides of this relationship, backer and project, are keeping track of each other, and to maintain consistent data, if one side is updated, the other side should be as well.
+#But... do you see the problem? Both sides of this relationship, backer and project, are keeping track of each other, and to maintain consistent data, if one side is updated, the other side should be as well.
 
-Whenever back_project() is called to update @backed_projects, the project should alsoupdate its @backers list.
+#Whenever back_project() is called to update @backed_projects, the project should alsoupdate its @backers list.
 
-Similarly, whenever add_backer() is called to update @backers, the backer should also update its `@backedprojects` list_.
+#Similarly, whenever add_backer() is called to update @backers, the backer should also update its `@backedprojects` list_.
