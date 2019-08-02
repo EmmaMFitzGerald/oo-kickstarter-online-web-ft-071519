bb458d467b0542c1fffaf81b5e22b2467b2ceaa7
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-02 15:12:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
   
-  attr_reader :name, :backed_projects, :project
+  attr_reader :name, :backed_projects
   
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   
-  def backed_project(project)
+  def back_project(project)
     @backed_projects <<  project
   end
   
