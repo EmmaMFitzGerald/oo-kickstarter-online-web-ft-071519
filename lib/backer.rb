8a56bd171f08992f291ecaf53543d4c03510cda4
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-02 15:43:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,8 +11,8 @@ class Backer
     @backed_projects << project
   end
   
-  
-end 
+end
+
 #But... do you see the problem? Both sides of this relationship, backer and project, are keeping track of each other, and to maintain consistent data, if one side is updated, the other side should be as well.
 
 #Whenever back_project() is called to update @backed_projects, the project should alsoupdate its @backers list.
